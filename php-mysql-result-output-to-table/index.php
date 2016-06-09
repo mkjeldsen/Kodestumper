ae42e7df6b74cae30834c7a4090b2170833f8305
Michael Kjeldsen <exp@firewerx.dk>
2016-06-09 14:29:01 +0200

    Create index.php

---
+++ php-mysql-result-output-to-table/index.php
@@ -0,0 +1,63 @@
+<?php
+	function mysql_output_result_table($sql,$db,$output_to='html') {
+		$rs = $db->query($sql);
+		$cols = null;
+
+		if ( $output_to == 'console' ) {
+			echo "<script>\n";
+			echo "// SQL: " . $sql . "\n";
+			echo "var row = [];\n";
+			while ( $row = $rs->fetch_assoc() ) {
+				if ( ! $cols) {
+					$cols = array_keys($row);
+
+					foreach ($cols as $col) {
+						$vars[] = $col;
+						$vars_out .= $col . ",";
+					}
+					$vars_out = rtrim($vars_out,",");
+					foreach ($vars as $var) {
+						$this_out .= "\tthis." . $var . " = " . $var . ";\n";
+					}
+					echo "function Row(" . $vars_out . ") {\n";
+					echo $this_out;
+					echo "}\n\n";
+				}
+
+				foreach ($cols as $col) {
+					$cells[] = $row[$col];
+				}
+//				var_dump($cells);
+				foreach ($cells as $cell) {
+					$cells_out .= "'" . $cell . "',";
+				}
+//				var_dump($cells_out);
+				$cells_out = rtrim($cells_out,",");
+				echo "row.push( new Row(" . $cells_out . ") );\n";
+				$cells = "";
+				$cells_out = "";
+			}
+			echo "console.table(row);\n\n";
+			echo "</script>";
+		} else {
+			while ( $row = $rs->fetch_assoc() ) {
+				if ( ! $cols) {
+					$cols = array_keys($row);
+
+					echo "<table>\n<thead>\n<tr>";
+					foreach ($cols as $col) {
+						echo "<th>$col</th>\n";
+					}
+					echo "</tr>\n<thead>\n<tbody>";
+				}
+
+				echo "<tr>\n";
+
+				foreach ($cols as $col) {
+					echo '<td>', $row[$col], "</td>\n";
+				}
+				echo "</tr>\n";
+			}
+			echo "</tbody>\n<caption><code style='text-align:left'>" . $sql . "</code></caption>\n</table>";
+		}
+	}
